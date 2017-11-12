a828b5cee8b3754c72b7fa2ed4f2224f3cc2b5e2
Ben Gracewood <ben@vendhq.com>
2017-11-12 15:17:26 +0000

    Fix year selector, add reset css

---
+++ src/components/YearSelector.js
@@ -0,0 +1,48 @@
+import React, { Component } from "react";
+
+import type { Year } from "../constants";
+import { years } from "../constants";
+
+type Props = {
+  setFocusedYearFn: Year => void
+};
+
+class YearSelector extends Component<Props> {
+  render() {
+    return (
+      <div className="YearSelector">
+        <style jsx="true">{`
+            .year-selector {
+                align: left;
+            }
+            li {
+                list-style: none;
+                display: inline-block;
+                margin: 0 5px;
+            }
+            h2 {
+                cursor: pointer;
+            }
+        `}</style>
+        <div className="year-selector">
+          <p>Nostalgia:</p>
+          <ul>
+            {years.slice().map(year => {
+              return (
+                <li key={year}>
+                  <h2
+                    onClick={() => this.props.setFocusedYearFn(year)}
+                  >
+                    {year}
+                  </h2>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default YearSelector;
