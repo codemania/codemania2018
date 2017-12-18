d448d6a8de039305bb2bc0853193a3731ea5bccd
Ben Gracewood <ben@vendhq.com>
2017-12-18 20:20:01 +1300

    New kitty image

--- src/components/KittersPyramid.js
+++ src/components/KittersPyramid.js
@@ -1,34 +1,20 @@
 import React, { Component } from 'react'
 
-import kitters from "../images/2018/kitters.svg";
-import pyramid from "../images/2018/top-pyramid.svg";
+import kitters from "../images/2018/kitters-top.svg";
 
 export default class KittersPyramid extends Component {
   render() {
     return (
       <div className="kitty">
         <style jsx="true">{`
-          .kittyImage {
+          .kitty {
             background: url(`+kitters+`) no-repeat center;
           }
-          .topPyramid {
-            background: url(`+pyramid+`) no-repeat center;
-            margin-top: -120px;
-          }
-
-          .image-break {
-            width: 70%;
-            height: 400px;
-            margin: 0 auto;
-          }
-          
           @media only screen
           and (max-width : 600px) {
             
           }
         `}</style>
-        <div className="kittyImage image-break" />
-        {/* <div className="topPyramid image-break" /> */}
       </div>
     )
   }
