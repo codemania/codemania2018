b3d6d397d1472a5214db5c09a14b322220da7fea
Ben Gracewood <ben@vendhq.com>
2017-12-20 20:06:34 +1300

    Speaker images and speaker page layout

--- src/components/SpeakerPage.js
+++ src/components/SpeakerPage.js
@@ -8,25 +8,78 @@ class SpeakerPage extends Component {
     var speakerId = this.props.match.params.speaker;
     console.log(speakerId);
     var speaker = speakers[yearId].speakers.find((s) => s.id === speakerId);
+    const speakerImage = require("../images/2018/speakers/" + speaker.id + ".png")
     return (
       <div className="speaker-page">
         <style jsx="true">{`
-          .speaker-page {
-            color: #fff;
+          .bg {
+            background: #fff;
           }
-          .speaker-page h2 {
+          .speaker-page {
+            color: #ff595a;
+            margin: 40px auto;
+            display: flex;
+          }
+          .speaker-page h1 {
             text-transform: uppercase;
             font-weight: bold;
+            font-size: 2em;
+            margin-left: 2px;
           }
-          .speaker-page p {
-             margin: 10px 0;
+          .speaker-details {
+            padding: 60px 20px 0 20px;
           }
+          .speaker-byline {
+            background: #ff595a;
+            color: #fff;
+            text-transform: uppercase;
+            margin-bottom: 1em;
+            padding: 5px 0 25px 5px;
+            font-weight: bold;
+            font-size: 0.9em;
+            line-height: 1.1em;
+          }
+          .speaker-byline p a {
+            color: #ffcd00;
+            text-decoration:  none;
+          }
+          .talk-details p {
+            margin-left: 3px;
+            font-size: smaller;
+            margin-bottom: 1em;
+            line-height: 1.1em;
+          }
+          .speaker-byline p a:hover {
+            text-decoration: underline;
+          }
+          @media only screen
+            and (max-width : 600px) {
+            .speaker-page {
+              flex-direction: column;
+            }
+            .speaker-image {
+              text-align: center;
+            }
+            .speaker-details {
+              padding: 20px 10px;
+            }
+          }
+
         `}</style>
-        <h2>{speaker.name}</h2>
-        <p>{speaker.bio}</p>
-        <p><a href={'https://twitter.com/'+speaker.twitter} target="_blank">@{speaker.twitter}</a></p>
-        <p>{speaker.talkTopic}</p>
-        <p>{speaker.talkBlurb}</p>
+        <div className="speaker-image">
+          <img src={speakerImage} alt={speaker.name} />
+        </div>
+        <div className="speaker-details">
+          <h1>{speaker.name}</h1>
+          <div className="speaker-byline">
+            <p>{speaker.bio}</p>
+            <p><a href={'https://twitter.com/' + speaker.twitter} target="_blank">@{speaker.twitter}</a></p>
+          </div>
+          <div class="talk-details">
+            <p>{speaker.talkTopic}</p>
+            <p>{speaker.talkBlurb}</p>
+          </div>
+        </div>
       </div>
     );
   }
