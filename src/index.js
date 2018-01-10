e4ad927fa8fef311f0fd4d7022d017f4275d3617
Ben Gracewood <ben@vendhq.com>
2018-01-10 19:42:06 +1300

    Unregister serviceworker because we don't need it

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import './index.css';
 import CodemaniaApp from './components/CodemaniaApp';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import registerServiceWorker from './registerServiceWorker';
+//import registerServiceWorker from './registerServiceWorker';
+import { unregister } from './registerServiceWorker';
 
 ReactDOM.render(<CodemaniaApp />, document.getElementById('root'));
-registerServiceWorker();
+unregister();
