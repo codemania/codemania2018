718a4c2d8d5b6e0f60ca6a217a4a728b7bf94054
Ian Randall <ihrandall@gmail.com>
2019-01-08 18:08:09 +1300

    Update the Agenda 2019 styling
    and wipe out the 2018 content

--- src/components/Agenda.js
+++ src/components/Agenda.js
@@ -1,7 +1,127 @@
 import React, { Component } from "react";
 
-
 const AgendaStyle = () => {
+  return (
+    <style jsx="true">{`
+      .bg {
+        background: #fff;
+      }
+      .agenda {
+        color: #44017d;
+        margin: 40px auto;
+      }
+      .agenda h1 {
+        text-transform: uppercase;
+        font-weight: bold;
+        font-size: 2em;
+      }
+      .agenda-header {
+        background: #44017d;
+        color: #fff;
+        text-transform: uppercase;
+        padding: 5px 0 25px 5px;
+        margin: 25px 0;
+        font-weight: bold;
+        font-size: 0.9em;
+        line-height: 1.1em;
+      }
+
+      .agenda-header:nth-child(5) {
+        margin-top: 1em;
+      }
+
+      .agenda-header h1, .agenda-header h2 {
+        font-size: 0.9em;
+        line-height: 1.1em;
+        margin: 0;
+      }
+      .agenda-header h2 {
+        color: #ff8f00;
+      }
+      .agenda p {
+        font-size: smaller;
+        margin-bottom: 1em;
+        line-height: 1.1em;
+      }
+      .agenda p a, .agenda .hilite {
+        color: #ff8f00;
+        text-decoration:  none;
+      }
+      .agenda p a:hover {
+        text-decoration:  underline;
+      }
+
+      #programme {
+        /* margin:0 25%; */
+        text-transform: capitalize;
+        border-spacing: 20px 0;
+    }
+
+    /*#programme tr:nth-child(even) td {
+        background: rgba(255, 255, 255, .2);
+    }*/
+
+    #programme-container {
+      padding: 20px;
+      background: #44017d;
+    }
+
+    #programme {
+      width: 100%;
+      padding: 20px;
+    }
+
+    #programme td {
+        border-top: 1px solid #fff;
+        padding: 3px;
+        width: 49%;
+    }
+
+    #programme .blank {
+        border: 0;
+        background: transparent;
+    }
+
+    #programme td:first-child {
+        border: 0;
+        width: 0;
+    }
+
+    #programme p {
+        color: #ffe5cf;
+        font-size: .5em;
+        margin: 0;
+        line-height: 1.4em;
+    }
+
+    #programme .speaker {
+        color: #fff;
+        font-size:.8em;
+    }
+
+    #programme .time {
+        color: #fff;
+        font-size:.5em;
+        text-align: right;
+    }
+
+      @media only screen
+        and (max-width : 600px) {
+          .agenda {
+            margin: 10px;
+          }
+          .agenda h1 {
+            font-size: 1.4em;
+          }
+          .agenda-header h1 {
+            font-size: 0.9em;
+          }
+        }
+    `}</style>
+  );
+}
+
+const AgendaStyleOld = () => {
   return (
     <style jsx="true">{`
       .bg {
@@ -61,8 +181,6 @@ const AgendaStyle = () => {
     /*#programme tr:nth-child(even) td {
         background: rgba(255, 255, 255, .2);
     }*/
-
-
 
     #programme-container {
       padding: 20px;
@@ -143,8 +261,8 @@ const AgendaContent = () => {
               <p class="time">09.00</p>
             </td>
             <td colSpan="2">
-              <p class="speaker"><a href="/speakers/2018/michael">Michael Lopp</a></p>
-              <p>THE SECOND ACT</p>
+              <p class="speaker"><a href="/speakers/2018/michael">Keynote 1</a></p>
+              <p>TBC</p>
             </td>
           </tr>
           <tr>
@@ -152,8 +270,8 @@ const AgendaContent = () => {
               <p class="time">09.50</p>
             </td>
             <td  colSpan="2">
-              <p class="speaker"><a href="/speakers/2018/sacha">Sacha Judd</a></p>
-              <p>SUPERFAN!</p>
+              <p class="speaker"><a href="/speakers/2018/sacha">Keynote 2</a></p>
+              <p>TBC</p>
             </td>
           </tr>
           <tr>
@@ -169,12 +287,12 @@ const AgendaContent = () => {
               <p class="time">11.15</p>
             </td>
             <td>
-              <p class="speaker"><a href="/speakers/2018/buzzy">Richard Busby</a></p>
-              <p>CONTAINER AUTOMATION, SECURITY, AND MONITORING ON AWS</p>
+              <p class="speaker"><a href="/speakers/2018/buzzy">Track 1 - 1</a></p>
+              <p>TBC</p>
             </td>
             <td>
-              <p class="speaker"><a href="/speakers/2018/erin">Erin McKean</a></p>
-              <p>BUILD AN API IN 20 MINUTES WITH LOOPBACK</p>
+              <p class="speaker"><a href="/speakers/2018/erin">Track 2 - 1</a></p>
+              <p>TBC</p>
             </td>
           </tr>
           <tr>
@@ -182,12 +300,12 @@ const AgendaContent = () => {
               <p class="time">12.00</p>
             </td>
             <td>
-              <p class="speaker"><a href="/speakers/2018/tammy">Tammy Butow</a></p>
-              <p>HOW TO ESTABLISH AN INCIDENT MANAGEMENT PROGRAM</p>
+              <p class="speaker"><a href="/speakers/2018/tammy">Track 1 - 2</a></p>
+              <p>TBC</p>
             </td>
             <td>
-              <p class="speaker"><a href="/speakers/2018/jana">Jana Beck</a></p>
-              <p>ESCAPING THE DIVING BELL</p>
+              <p class="speaker"><a href="/speakers/2018/jana">Track 2 - 2</a></p>
+              <p>TBC</p>
             </td>
           </tr>
           <tr>
@@ -203,12 +321,12 @@ const AgendaContent = () => {
               <p class="time">13.45</p>
             </td>
             <td>
-              <p class="speaker"><a href="/speakers/2018/katie-bell">Katie Bell</a></p>
-              <p>KUBERNETES: DISTRIBUTED SYSTEMS MADE EASY(-IER)</p>
+              <p class="speaker"><a href="/speakers/2018/katie-bell">Track 1 - 3</a></p>
+              <p>TBC</p>
             </td>
             <td>
-              <p class="speaker"><a href="/speakers/2018/mark">Mark Young</a></p>
-              <p>THE TENTATIVE MUD-TENDER</p>
+              <p class="speaker"><a href="/speakers/2018/mark">Track 2 - 3</a></p>
+              <p>TBC</p>
             </td>
           </tr>
           <tr>
@@ -216,12 +334,12 @@ const AgendaContent = () => {
               <p class="time">14.35</p>
             </td>
             <td>
-              <p class="speaker"><a href="/speakers/2018/patima">Patima Tantiprasut</a></p>
-              <p>THE ART OF GIVING A DAMN, AND ALSO NOT</p>
+              <p class="speaker"><a href="/speakers/2018/patima">Track 1 - 4</a></p>
+              <p>TBC</p>
             </td>
             <td>
-              <p class="speaker"><a href="/speakers/2018/karl">Karl von Randow</a></p>
-              <p>THE STORY OF CHARLES ON IOS</p>
+              <p class="speaker"><a href="/speakers/2018/karl">Track 2 - 4</a></p>
+              <p>TBC</p>
             </td>
           </tr>
           <tr>
@@ -237,8 +355,8 @@ const AgendaContent = () => {
               <p class="time">16.00</p>
             </td>
             <td colspan="2">
-              <p class="speaker"><a href="/speakers/2018/christine">Christine Yen</a></p>
-              <p>OBSERVABILITY AND THE DEVELOPMENT PROCESS</p>
+              <p class="speaker"><a href="/speakers/2018/christine">Locknote 1</a></p>
+              <p>TBC</p>
             </td>
           </tr>
           <tr>
@@ -246,8 +364,8 @@ const AgendaContent = () => {
               <p class="time">16.50</p>
             </td>
             <td colspan="2">
-              <p class="speaker"><a href="/speakers/2018/richard">Rich Campbell</a></p>
-              <p>THE MOON: GATEWAY TO THE SOLAR SYSTEM</p>
+              <p class="speaker"><a href="/speakers/2018/richard">Locknote 2</a></p>
+              <p>TBC</p>
             </td>
           </tr>
           <tr>
@@ -270,7 +388,7 @@ class Agenda extends Component {
   render() {
     return (
       <div className="agenda" id="agenda">
-        <AgendaStyle />
+      {this.props.is2019 === "true" ? <AgendaStyle /> : <AgendaStyleOld />}
         <h1>Agenda</h1>
         <div className="agenda-header">
           <h1>Codemania Conference</h1>
