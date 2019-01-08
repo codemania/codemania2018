f7fa69f31ce1d713432f9d03aa0b76953f184b9d
Ian Randall <ihrandall@gmail.com>
2019-01-08 18:37:41 +1300

    Add the 2019 styling to SpeakerPage

--- src/components/SpeakerPage.js
+++ src/components/SpeakerPage.js
@@ -2,22 +2,78 @@ import React, { Component } from "react";
 
 import { speakers } from "../constants";
 
-class SpeakerPage extends Component {
-  render() {
-    var yearId = this.props.match.params.year;
-    var speakerId = this.props.match.params.speaker;
-    console.log(speakerId);
-    var speaker = speakers[yearId].speakers.find((s) => s.id === speakerId);
-    var speakerImage
-    try {
-      speakerImage = require("../images/2018/speakers/round/" + speaker.id + ".png")
-    } catch(err) {
-      speakerImage = require("../images/2018/speakers/round/default.png")
-    }
-    return (
-      <div className="speaker-page">
-        <style jsx="true">{`
-          .bg {
+const SpeakerPageStyle = () => {
+  return (
+    <style jsx="true">{`
+      .bg {
+        background: #fff;
+      }
+      .speaker-page {
+        color: #44017d;
+        margin: 40px auto;
+        display: flex;
+      }
+      .speaker-page h1 {
+        text-transform: uppercase;
+        font-weight: bold;
+        font-size: 2em;
+        margin-left: 2px;
+      }
+      .speaker-details {
+        padding: 60px 20px 0 20px;
+      }
+      .speaker-byline {
+        background: #44017d;
+        color: #fff;
+        text-transform: uppercase;
+        margin-bottom: 1em;
+        padding: 5px 5px 20px 5px;
+        font-weight: bold;
+        font-size: 0.9em;
+        line-height: 1.1em;
+      }
+      .speaker-byline p a {
+        color: #ffcd00;
+        text-decoration:  none;
+      }
+      .talk-details p {
+        margin-left: 3px;
+        font-size: smaller;
+        margin-bottom: 1em;
+        line-height: 1.1em;
+      }
+      .talk-details p b {
+        text-transform: uppercase;
+      }
+      .talk-details p a, .talk-details .hilite {
+        color: #ffcd00;
+        text-decoration:  none;
+      }
+      .talk-details p a:hover {
+        text-decoration:  underline;
+      }
+      .speaker-byline p a:hover {
+        text-decoration: underline;
+      }
+      @media only screen
+        and (max-width : 600px) {
+        .speaker-page {
+          flex-direction: column;
+        }
+        .speaker-image {
+          text-align: center;
+        }
+        .speaker-details {
+          padding: 20px 10px;
+        }
+      }
+    `}</style>);
+};
+
+const SpeakerPageStyleOld = () => {
+  return (
+    <style jsx="true">{`
+      .bg {
             background: #fff;
           }
           .speaker-page {
@@ -79,8 +135,26 @@ class SpeakerPage extends Component {
               padding: 20px 10px;
             }
           }
+    `}</style>);
+};
 
-        `}</style>
+class SpeakerPage extends Component {
+
+  render() {
+    var yearId = this.props.match.params.year;
+    var speakerId = this.props.match.params.speaker;
+    var is2019 = process.env.REACT_APP_FLAGS_IS2019==="true";
+
+    var speaker = speakers[yearId].speakers.find((s) => s.id === speakerId);
+    var speakerImage
+    try {
+      speakerImage = require("../images/2018/speakers/round/" + speaker.id + ".png")
+    } catch(err) {
+      speakerImage = require("../images/2018/speakers/round/default.png")
+    }
+    return (
+      <div className="speaker-page">
+        {is2019 ? <SpeakerPageStyle /> : <SpeakerPageStyleOld />}
         <div className="speaker-image">
           <img src={speakerImage} alt={speaker.name} />
         </div>
