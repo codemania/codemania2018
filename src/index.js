ef81eed1cd0abfd35372db85c2456eaa3b45b7ee
Ben Gracewood <ben@vendhq.com>
2019-01-10 10:34:03 +1300

    Kill the service worker to bust cache

--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ import './index.css';
 import CodemaniaApp from './components/CodemaniaApp';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import registerServiceWorker from './registerServiceWorker';
+import { unregister } from './registerServiceWorker';
 
 require('dotenv').config();
 
 ReactDOM.render(<CodemaniaApp />, document.getElementById('root'));
-registerServiceWorker();
+unregister()
