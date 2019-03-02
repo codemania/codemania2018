a78585050031cf7d2784c3a455c3dce8ff8acbc2
Ian Randall <ihrandall@gmail.com>
2019-03-03 10:06:38 +1300

    Remove the flag_is2019 logic

--- src/components/Agenda.js
+++ src/components/Agenda.js
@@ -121,127 +121,6 @@ const AgendaStyle = () => {
   );
 }
 
-const AgendaStyleOld = () => {
-  return (
-    <style jsx="true">{`
-      .bg {
-        background: #fff;
-      }
-      .agenda {
-        color: #ff595a;
-        margin: 40px auto;
-      }
-      .agenda h1 {
-        text-transform: uppercase;
-        font-weight: bold;
-        font-size: 2em;
-      }
-      .agenda-header {
-        background: #ff595a;
-        color: #fff;
-        text-transform: uppercase;
-        padding: 5px 0 25px 5px;
-        margin: 25px 0;
-        font-weight: bold;
-        font-size: 0.9em;
-        line-height: 1.1em;
-      }
-
-      .agenda-header:nth-child(5) {
-        margin-top: 1em;
-      }
-
-      .agenda-header h1, .agenda-header h2 {
-        font-size: 0.9em;
-        line-height: 1.1em;
-        margin: 0;
-      }
-      .agenda-header h2 {
-        color: #ffcd00;
-      }
-      .agenda p {
-        font-size: smaller;
-        margin-bottom: 1em;
-        line-height: 1.1em;
-      }
-      .agenda p a, .agenda .hilite {
-        color: #ffcd00;
-        text-decoration:  none;
-      }
-      .agenda p a:hover {
-        text-decoration:  underline;
-      }
-
-      #programme {
-        /* margin:0 25%; */
-        text-transform: capitalize;
-        border-spacing: 20px 0;
-    }
-
-    /*#programme tr:nth-child(even) td {
-        background: rgba(255, 255, 255, .2);
-    }*/
-
-    #programme-container {
-      padding: 20px;
-      background: #ff595a;
-    }
-
-    #programme {
-      width: 100%;
-      padding: 20px;
-    }
-
-    #programme td {
-        border-top: 1px solid #fff;
-        padding: 3px;
-        width: 49%;
-    }
-
-    #programme .blank {
-        border: 0;
-        background: transparent;
-    }
-
-    #programme td:first-child {
-        border: 0;
-        width: 0;
-    }
-
-    #programme p {
-        color: #ffe5cf;
-        font-size: .5em;
-        margin: 0;
-        line-height: 1.4em;
-    }
-
-    #programme .speaker {
-        color: #fff;
-        font-size:.8em;
-    }
-
-    #programme .time {
-        color: #fff;
-        font-size:.5em;
-        text-align: right;
-    }
-
-      @media only screen
-        and (max-width : 600px) {
-          .agenda {
-            margin: 10px;
-          }
-          .agenda h1 {
-            font-size: 1.4em;
-          }
-          .agenda-header h1 {
-            font-size: 0.9em;
-          }
-        }
-    `}</style>
-  );
-}
-
 const AgendaContent = () => {
   return (
     <div id="programme-container">
@@ -386,18 +265,15 @@ const AgendaContent = () => {
 class Agenda extends Component {
 
   render() {
-    var flag_is2019 = process.env.REACT_APP_FLAGS_IS2019==="true";
     return (
       <div className="agenda" id="agenda">
-      {flag_is2019 ? <AgendaStyle /> : <AgendaStyleOld />}
+        <AgendaStyle />
         <h1>Agenda</h1>
         <div className="agenda-header">
           <h1>Codemania Conference</h1>
           <h2>Wednesday 15th May 2019</h2>
         </div>
-
-        {flag_is2019 ? <AgendaContent /> : <h2>2019 ROSTER TBC</h2>}
-
+        <AgendaContent />
         <div className="agenda-header">
           <h1>Codemania Conversations</h1>
           <h2>Thursday 16th May - Friday 17th May 2018</h2>
--- src/components/SpeakerPage.js
+++ src/components/SpeakerPage.js
@@ -70,80 +70,11 @@ const SpeakerPageStyle = () => {
     `}</style>);
 };
 
-const SpeakerPageStyleOld = () => {
-  return (
-    <style jsx="true">{`
-      .bg {
-            background: #fff;
-          }
-          .speaker-page {
-            color: #ff595a;
-            margin: 40px auto;
-            display: flex;
-          }
-          .speaker-page h1 {
-            text-transform: uppercase;
-            font-weight: bold;
-            font-size: 2em;
-            margin-left: 2px;
-          }
-          .speaker-details {
-            padding: 60px 20px 0 20px;
-          }
-          .speaker-byline {
-            background: #ff595a;
-            color: #fff;
-            text-transform: uppercase;
-            margin-bottom: 1em;
-            padding: 5px 0 25px 5px;
-            font-weight: bold;
-            font-size: 0.9em;
-            line-height: 1.1em;
-          }
-          .speaker-byline p a {
-            color: #ffcd00;
-            text-decoration:  none;
-          }
-          .talk-details p {
-            margin-left: 3px;
-            font-size: smaller;
-            margin-bottom: 1em;
-            line-height: 1.1em;
-          }
-          .talk-details p b {
-            text-transform: uppercase;
-          }
-          .talk-details p a, .talk-details .hilite {
-            color: #ffcd00;
-            text-decoration:  none;
-          }
-          .talk-details p a:hover {
-            text-decoration:  underline;
-          }
-          .speaker-byline p a:hover {
-            text-decoration: underline;
-          }
-          @media only screen
-            and (max-width : 600px) {
-            .speaker-page {
-              flex-direction: column;
-            }
-            .speaker-image {
-              text-align: center;
-            }
-            .speaker-details {
-              padding: 20px 10px;
-            }
-          }
-    `}</style>);
-};
-
 class SpeakerPage extends Component {
 
   render() {
     var yearId = this.props.match.params.year;
     var speakerId = this.props.match.params.speaker;
-    var flag_is2019 = process.env.REACT_APP_FLAGS_IS2019==="true";
 
     var speaker = speakers[yearId].speakers.find((s) => s.id === speakerId);
     var speakerImage
@@ -154,7 +85,7 @@ class SpeakerPage extends Component {
     }
     return (
       <div className="speaker-page">
-        {flag_is2019 ? <SpeakerPageStyle /> : <SpeakerPageStyleOld />}
+        <SpeakerPageStyle />
         <div className="speaker-image">
           <img src={speakerImage} alt={speaker.name} />
         </div>
